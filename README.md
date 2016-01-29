8aa27e3f96fd4f596970bc41376a6e62c4fed7cc
Brandon Nicholls <brandon.nicholls@gmail.com>
2016-01-28 23:03:37 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # eslint-plugin-contains
+========================
 An ESLint rule which will search for a given string and fail with the given error if not found
+
+**To use**:
+`npm install eslint-plugin-contains --save-dev`
+
+Modify your .eslintrc file:
+* Add the plugin to the plugins array like this:
+```
+    "plugins": [
+        "contains"
+    ]
+```
+* Add the rule to the rules object like this:
+```
+    "rules": {
+        "contains/contains": [2, { "toFind": "this is the text to look for", "errorMessage": "An appropriate error message when not found"} ]
+    }
+```
+
+Now run eslint.
---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  rules: {
+    "contains": function (context) {
+      if (context.options.length === 1) {
+        var options = context.options[0];
+        if (!options.toFind || !options.errorMessage) {
+          return {};
+        }
+
+        var sourceCode = context.getSourceCode();
+        var fileContent = sourceCode.getText();
+
+        if (fileContent.indexOf(options.toFind) < 0) {
+          context.report({
+            message: options.errorMessage,
+            loc: {
+              line: 1,
+              col: 1
+            }
+          });
+        }
+      }
+
+      return {};
+    }
+  }
+};
